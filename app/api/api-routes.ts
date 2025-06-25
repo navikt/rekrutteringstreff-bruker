341b80ecd32a0241d46d16cf418acfcc43bba74e
haugse <sara.haugse@gmail.com>
2025-06-25 12:18:07 +0200

    Fjern variabel som ikke brukes

--- app/api/api-routes.ts
+++ app/api/api-routes.ts
@@ -1,13 +1,11 @@
 import { getCluster } from '../util';
 
 const gcp = getCluster();
-// const fss = getCluster(true);
 
 export interface Iroute {
   api_route: string;
   api_url: string;
   internUrl: string;
-  scope: string;
   audience: string;
 }
 
@@ -15,6 +13,5 @@ export const RekrutteringstreffMinSide: Iroute = {
   api_route: '/api',
   api_url: process.env.REKRUTTERINGSTREFF_MINSIDE_API ?? '',
   internUrl: '/api/rekrutteringstreff-minside',
-  scope: `api://${gcp}.toi.rekrutteringstreff-minside-api/.default`,
   audience: `${gcp}:toi:rekrutteringstreff-minside-api`,
 };
