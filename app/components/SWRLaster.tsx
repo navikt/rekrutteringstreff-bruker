dc8880a3320bb63574320d5b3d8b2faa25d9001a
Ken Ove Andersen <ken.ove.andersen@nav.no>
2025-07-03 14:48:21 +0200

    Debugging
    Co-authored-by: Sara <43448582+haugse@users.noreply.github.com>

--- app/components/SWRLaster.tsx
+++ app/components/SWRLaster.tsx
@@ -37,26 +37,31 @@ const SWRLaster = <T extends any[]>({
   }
 
   const error = hooks.find((hook) => hook?.error)?.error;
-  if (error && egenFeilmelding) {
-    return <>{egenFeilmelding(error)}</>;
-  }
 
-  console.log("error", error);
+  console.log("error.name", error?.name);
+  console.log("error", JSON.stringify(error));
   if (error instanceof Response && error.status === 401) {
     const loginUrl = process.env.LOGIN_URL;
     console.log("loginUrl", loginUrl);
     window.location.href = `${loginUrl}?redirect=${window.location.pathname}`
+  } else {
+
+    if (error && egenFeilmelding) {
+      return <>{egenFeilmelding(error)}</>;
+    }
+
+    if (error && !skjulFeilmelding) {
+      console.warn(error);
+      return <div> Feil ved henting av data </div>;
+    }
+
+    if (hooks.every((hook) => hook?.data)) {
+      const data = hooks.map((hook) => hook?.data) as T;
+      return <>{children(...data)}</>;
+    }
+
   }
 
-  if (error && !skjulFeilmelding) {
-    console.warn(error);
-    return <div> Feil ved henting av data </div>;
-  }
-
-  if (hooks.every((hook) => hook?.data)) {
-    const data = hooks.map((hook) => hook?.data) as T;
-    return <>{children(...data)}</>;
-  }
 
   return null;
 };
