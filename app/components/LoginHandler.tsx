8a0b524ec7d1268263083f88a83cc5797120a06e
Erlend Wiklem <admin@wiklem.no>
2026-01-12 12:53:15 +0100

    wip

--- app/components/LoginHandler.tsx
+++ app/components/LoginHandler.tsx
@@ -1,36 +1,39 @@
 'use client';
 
-import {useEffect} from "react";
+import { useEffect } from 'react';
 
-export default function LoginHandler(){
+export default function LoginHandler() {
+  const sessionUrl =
+    process.env.NEXT_PUBLIC_SESSION_URL ||
+    'https://login.ekstern.dev.nav.no/oauth2/session';
+  const loginUrl =
+    process.env.NEXT_PUBLIC_LOGIN_URL ||
+    'https://login.ekstern.dev.nav.no/oauth2/login';
 
-    const sessionUrl= process.env.NEXT_PUBLIC_SESSION_URL || 'https://login.ekstern.dev.nav.no/oauth2/session';
-    const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL || 'https://login.ekstern.dev.nav.no/oauth2/login';
+  const fetchSessionInfo = async () => {
+    console.log(`Henter session fra ${sessionUrl} og loginUrl ${loginUrl}`);
+    const response = await fetch(`${sessionUrl}`, {
+      credentials: 'include',
+    }).catch((e) => {
+      console.error(
+        'Det oppstod en feil ved henting av session status',
+        e.message,
+      );
+    });
+    if (!response) return;
 
-    const fetchSessionInfo = async () => {
-        console.log(`Henter session fra ${sessionUrl} og loginUrl ${loginUrl}`);
-        const response = await fetch(`${sessionUrl}`, {
-            credentials: 'include',
-        }).catch((e) => {
-            console.error('Det oppstod en feil ved henting av session status', e.message);
-        });
-        if (!response) return;
+    if (response.status === 401) {
+      const rekrutteringstreffId = window.location.pathname.split('/').at(-1);
+      console.log(`RekrutteringstreffId: ${rekrutteringstreffId}`);
 
-        if (response.status === 401) {
-            const rekrutteringstreffId = window.location.pathname.split('/').at(-1)
-            console.log(`RekrutteringstreffId: ${rekrutteringstreffId}`);
+      window.location.href = `${loginUrl}?redirect=${window.location.origin}/rekrutteringstreff/${rekrutteringstreffId}&level=Level3`;
+    }
+  };
 
-            window.location.href = `${loginUrl}?redirect=${window.location.origin}/rekrutteringstreff/${rekrutteringstreffId}&level=Level3`;
-        }
-    };
+  useEffect(() => {
+    console.log('Henter session info');
+    fetchSessionInfo();
+  }, [fetchSessionInfo]);
 
-    useEffect(() => {
-        console.log('Henter session info');
-        fetchSessionInfo()
-    }, [fetchSessionInfo]);
-
-    return (
-       <>
-       </>
-    )
+  return <></>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import './globals.css';
-import LoginHandler from '@/app/components/LoginHandler';
 import MirageInitializer from '@/app/components/MirageInitializer';
+import { UmamiProvider } from '@/app/providers/UmamiContext';
 import { isLocal } from '@/app/util';
 import { fetchDecoratorReact } from '@navikt/nav-dekoratoren-moduler/ssr';
 import type { Metadata } from 'next';
 import Script from 'next/script';
-import {isLocal} from "@/app/util";
-import MirageInitializer from "@/app/components/MirageInitializer";
-import {UmamiProvider} from "@/app/providers/UmamiContext";
 
 export const metadata: Metadata = {
   title: 'Rekrutteringstreff-bruker',
@@ -34,25 +31,34 @@ export default async function RootLayout({
     },
   });
 
-    return (
-        <html lang='no' className='h-full'>
-            <head>
-                <Decorator.HeadAssets />
-            </head>
-            <body style={{scrollbarGutter: 'stable'}}>
-                       <div data-pa11y-ignore='decorator-header'> <Decorator.Header />    </div>
-                <UmamiProvider>
-                    <BrukLokalMock>
-                        <main className='flex-grow flex flex-col contentContainer' style={{'scrollbarGutter': 'stable'}}>
-                            {children}
-                        </main>
-                    </BrukLokalMock>
-                </UmamiProvider>      </div>
-                   <div data-pa11y-ignore='decorator-footer'> <Decorator.Footer />
-                <Decorator.Scripts loader={Script} />
-            </body>
-        </html>
-    );
+  return (
+    <html lang='no' className='h-full'>
+      <head>
+        <Decorator.HeadAssets />
+      </head>
+      <body style={{ scrollbarGutter: 'stable' }}>
+        <div data-pa11y-ignore='decorator-header'>
+          {' '}
+          <Decorator.Header />{' '}
+        </div>
+        <UmamiProvider>
+          <BrukLokalMock>
+            <main
+              className='flex-grow flex flex-col contentContainer'
+              style={{ scrollbarGutter: 'stable' }}
+            >
+              {children}
+            </main>
+          </BrukLokalMock>
+        </UmamiProvider>
+        <div data-pa11y-ignore='decorator-footer'>
+          {' '}
+          <Decorator.Footer />
+        </div>
+        <Decorator.Scripts loader={Script} />
+      </body>
+    </html>
+  );
 }
 
 const BrukLokalMock = ({ children }: { children: React.ReactNode }) => {
