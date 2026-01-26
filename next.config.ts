b13fb86f7575a7fa3568e2c804236724e242cf14
Ken Ove Andersen <ken.ove.andersen@nav.no>
2026-01-26 11:36:11 +0100

    Bruk /rekrutteringstreff som basePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: '/rekrutteringstreff',
   transpilePackages: ['@navikt/ds-react', '@navikt/ds-css'],
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
