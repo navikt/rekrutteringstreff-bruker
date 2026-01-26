72d060ecf33772e32a5a3aaf511c2fc0fbebdbd4
Ken Ove Andersen <ken.ove.andersen@nav.no>
2026-01-26 12:16:29 +0100

    Prøv å endre url'er for å få kall til å bli riktige

--- app/api/api-routes.ts
+++ app/api/api-routes.ts
@@ -12,6 +12,6 @@ export interface Iroute {
 export const RekrutteringstreffMinSide: Iroute = {
   api_route: '/api',
   api_url: process.env.REKRUTTERINGSTREFF_MINSIDE_API ?? '',
-  internUrl: '/rekrutteringstreff/api/rekrutteringstreff-minside',
+  internUrl: '/api/rekrutteringstreff-minside',
   audience: `${gcp}:toi:rekrutteringstreff-minside-api`,
 };
