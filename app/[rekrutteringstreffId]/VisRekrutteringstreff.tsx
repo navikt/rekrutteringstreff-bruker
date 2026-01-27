8a1f560bfd9d9c17afe35fea82b948af81021fd6
Ken Ove Andersen <ken.ove.andersen@nav.no>
2026-01-27 15:18:16 +0100

    Endre spacing

--- app/[rekrutteringstreffId]/VisRekrutteringstreff.tsx
+++ app/[rekrutteringstreffId]/VisRekrutteringstreff.tsx
@@ -31,76 +31,80 @@ const VisRekrutteringstreff: React.FC<VisRekrutteringstreffProps> = ({rekrutteri
   return (
       <div className='mb-8 flex items-center gap-10'>
           <SWRLaster hooks={[enkeltRekrutteringstreffHook, enkeltRekrutteringstreffSvarHook]}>
-            {(rekrutteringstreff, enkeltRekrutteringstreffSvar) => {
-                if (!rekrutteringstreff) {
-                  logger.warn(`Fant ikke data for rekrutteringstreff med id: ${rekrutteringstreffId}`);
-                  return <div>Ingen data funnet for rekrutteringstreff med ID: {rekrutteringstreffId}</div>;
-                }
-                logger.info(`Viser rekrutteringstreff ${rekrutteringstreffId}`);
-                return (
-                    <Page className="min-w-full">
-                        <Page.Block as="main" width="xl" gutters>
-                            <HGrid columns={{  xs: "1", lg: "65% 35%" }} gap="space-0">
-                                <div >
-                                    <Heading size="medium" className="mb-6 mr-4">{rekrutteringstreff.tittel}</Heading>
-                                    <HGrid columns={{  xs: 1, lg: 2 }} gap="space-24" className="pb-4 text-base">
-                                        <Tid fraTid={rekrutteringstreff.fraTid} tilTid={rekrutteringstreff.tilTid}/>
-                                        <Sted gateadresse={rekrutteringstreff.gateadresse}
-                                              postnummer={rekrutteringstreff.postnummer}
-                                              poststed={rekrutteringstreff.poststed}
-                                        />
-                                    </HGrid>
-                                </div>
-                                <div>
-                                    <Svarboks erInvitert={enkeltRekrutteringstreffSvar.erInvitert}
-                                              erPåmeldt={enkeltRekrutteringstreffSvar.erPåmeldt}
-                                              harSvart={enkeltRekrutteringstreffSvar.harSvart}
-                                              svarfrist={rekrutteringstreff.svarfrist}
-                                              fraTid={rekrutteringstreff.fraTid}
-                                              tilTid={rekrutteringstreff.tilTid}
-                                              status={rekrutteringstreff.status}
-                                              laster={enkeltRekrutteringstreffSvarHook.isLoading}
-                                              rekrutteringstreffId={rekrutteringstreffId}
-                                              svarEndret={(svar) => {
-                                                  enkeltRekrutteringstreffSvarHook.mutate({erInvitert: true, erPåmeldt: svar, harSvart: true});
-                                              }}
-                                    />
-                                </div>
-                            </HGrid>
+              {(rekrutteringstreff, enkeltRekrutteringstreffSvar) => {
+                  if (!rekrutteringstreff) {
+                      logger.warn(`Fant ikke data for rekrutteringstreff med id: ${rekrutteringstreffId}`);
+                      return <div>Ingen data funnet for rekrutteringstreff med ID: {rekrutteringstreffId}</div>;
+                  }
+                  logger.info(`Viser rekrutteringstreff ${rekrutteringstreffId}`);
+                  return (
+                      <Page className="min-w-full">
+                          <Page.Block as="main" width="xl" gutters>
+                              <HGrid columns={{xs: "1", lg: "65% 35%"}} gap="space-0">
+                                  <div>
+                                      <Heading size="medium" className="mb-6 mr-4">{rekrutteringstreff.tittel}</Heading>
+                                      <HGrid columns={{xs: 1, lg: 2}} gap="space-24" className="pb-4 text-base">
+                                          <Tid fraTid={rekrutteringstreff.fraTid} tilTid={rekrutteringstreff.tilTid} />
+                                          <Sted gateadresse={rekrutteringstreff.gateadresse}
+                                                postnummer={rekrutteringstreff.postnummer}
+                                                poststed={rekrutteringstreff.poststed}
+                                          />
+                                      </HGrid>
+                                  </div>
+                                  <div>
+                                      <Svarboks erInvitert={enkeltRekrutteringstreffSvar.erInvitert}
+                                                erPåmeldt={enkeltRekrutteringstreffSvar.erPåmeldt}
+                                                harSvart={enkeltRekrutteringstreffSvar.harSvart}
+                                                svarfrist={rekrutteringstreff.svarfrist}
+                                                fraTid={rekrutteringstreff.fraTid}
+                                                tilTid={rekrutteringstreff.tilTid}
+                                                status={rekrutteringstreff.status}
+                                                laster={enkeltRekrutteringstreffSvarHook.isLoading}
+                                                rekrutteringstreffId={rekrutteringstreffId}
+                                                svarEndret={(svar) => {
+                                                    enkeltRekrutteringstreffSvarHook.mutate({
+                                                        erInvitert: true,
+                                                        erPåmeldt: svar,
+                                                        harSvart: true
+                                                    });
+                                                }}
+                                      />
+                                  </div>
+                              </HGrid>
 
-                            <Show below="lg" >
-                                <Tabs defaultValue="innlegg">
-                                    <Tabs.List>
-                                        <Tabs.Tab value="innlegg"
-                                                  label={`Siste aktivitet (${rekrutteringstreff.innlegg.length})`} />
-                                        <Tabs.Tab value="arbeidsgivere"
-                                                  label={`Arbeidsgivere (${rekrutteringstreff.arbeidsgivere.length})`} />
-                                    </Tabs.List>
-                                    <Tabs.Panel value="innlegg">
-                                        <InnleggListe innlegg={rekrutteringstreff.innlegg} />
-                                    </Tabs.Panel>
-                                    <Tabs.Panel value="arbeidsgivere">
-                                        <ArbeidsgiverListe arbeidsgivere={rekrutteringstreff.arbeidsgivere} />
-                                    </Tabs.Panel>
-                                </Tabs>
-                            </Show>
+                              <Show below="lg">
+                                  <Tabs defaultValue="innlegg">
+                                      <Tabs.List>
+                                          <Tabs.Tab value="innlegg"
+                                                    label={`Siste aktivitet (${rekrutteringstreff.innlegg.length})`} />
+                                          <Tabs.Tab value="arbeidsgivere"
+                                                    label={`Arbeidsgivere (${rekrutteringstreff.arbeidsgivere.length})`} />
+                                      </Tabs.List>
+                                      <Tabs.Panel value="innlegg">
+                                          <InnleggListe innlegg={rekrutteringstreff.innlegg} />
+                                      </Tabs.Panel>
+                                      <Tabs.Panel value="arbeidsgivere">
+                                          <ArbeidsgiverListe arbeidsgivere={rekrutteringstreff.arbeidsgivere} />
+                                      </Tabs.Panel>
+                                  </Tabs>
+                              </Show>
 
-                            <Show above="lg">
-                                 <HGrid columns={ "65% 35%" }>
-                                    <div className="pr-8">
-                                        <Heading size="xsmall" className="mb-4">Siste aktivitet</Heading>
-                                        <InnleggListe innlegg={rekrutteringstreff.innlegg} />
-                                    </div>
-                                    <div>
-                                        <Heading size="xsmall" className="mb-4">Arbeidsgivere</Heading>
-                                        <ArbeidsgiverListe arbeidsgivere={rekrutteringstreff.arbeidsgivere} />
-                                    </div>
-                                 </HGrid>
-                            </Show>
-                        </Page.Block>
-                    </Page>
-                );
-            }}
+                              <Show above="lg">
+                                  <HGrid columns={"65% 35%"}>
+                                      <div className="pr-8">
+                                          <Heading size="xsmall" className="mb-4">Siste aktivitet</Heading>
+                                          <InnleggListe innlegg={rekrutteringstreff.innlegg} />
+                                      </div>
+                                      <div>
+                                          <Heading size="xsmall" className="mb-4">Arbeidsgivere</Heading>
+                                          <ArbeidsgiverListe arbeidsgivere={rekrutteringstreff.arbeidsgivere} />
+                                      </div>
+                                  </HGrid>
+                              </Show>
+                          </Page.Block>
+                      </Page>
+                  );
+              }}
           </SWRLaster>
       </div>
   );
