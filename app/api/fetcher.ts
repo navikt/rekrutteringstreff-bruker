5bf864881eb535df4e97384e30df336727f92d6a
Ken Ove Andersen <ken.ove.andersen@nav.no>
2026-02-27 16:03:14 +0100

    Legg til logging

--- app/api/fetcher.ts
+++ app/api/fetcher.ts
@@ -41,7 +41,7 @@ export const getAPIwithSchema = <T>(
       throw new Error('Non-JSON content received');
     }
 
-    logger.info(`GET response data for ${url}: ${jsonData}`);
+    logger.info(`GET response data for ${url}`, jsonData);
     return validerSchema(schema, jsonData);
   };
 };
