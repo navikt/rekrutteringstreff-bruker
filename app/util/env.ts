d32116912d14a9204923182731b2159e5e097568
Ken Ove Andersen <ken.ove.andersen@nav.no>
2026-03-09 12:16:26 +0100

    Endre formattering

--- app/util/env.ts
+++ app/util/env.ts
@@ -1,17 +1,17 @@
 export function getServerEnv(): {
-    SESSION_URL: string,
-    LOGIN_URL: string,
+  SESSION_URL: string;
+  LOGIN_URL: string;
 } {
-    if (process.env.NEXT_PUBLIC_SESSION_URL == null) {
-        throw new Error("Miljøvariabelen NEXT_PUBLIC_SESSION_URL er ikke satt");
-    }
+  if (process.env.NEXT_PUBLIC_SESSION_URL == null) {
+    throw new Error("Miljøvariabelen NEXT_PUBLIC_SESSION_URL er ikke satt");
+  }
 
-    if (process.env.NEXT_PUBLIC_LOGIN_URL == null) {
-        throw new Error("Miljøvariabelen NEXT_PUBLIC_LOGIN_URL er ikke satt");
-    }
+  if (process.env.NEXT_PUBLIC_LOGIN_URL == null) {
+    throw new Error("Miljøvariabelen NEXT_PUBLIC_LOGIN_URL er ikke satt");
+  }
 
-    return {
-        SESSION_URL: process.env.NEXT_PUBLIC_SESSION_URL as string,
-        LOGIN_URL: process.env.NEXT_PUBLIC_LOGIN_URL as string,
-    }
+  return {
+    SESSION_URL: process.env.NEXT_PUBLIC_SESSION_URL as string,
+    LOGIN_URL: process.env.NEXT_PUBLIC_LOGIN_URL as string,
+  }
 }
